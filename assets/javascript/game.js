860e42b4d379c8c887a91b2d89758c6b825e3520
Mads <madisonmartinez@Alicias-MBP.lan>
2019-05-08 22:43:21 +0000

    basic html and css starter

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+// Global Variables wins, loses, target score, your score
+var randomNum = generateRandom();
+
+//array of crystal opject (to loop through)
+
+//dynamically create crystals on the page (one object per crystal)
+   //each crystal needs a score, an image, [aka a string holding the path to the image - which is saved in a folder in your project] an object with all of this info (aka collection of key value parts)
+
+// Random number Function:
+function generateRandom()
+   //something with math...
+   //return a random number
+
+//Click event function
+   //each crystal should have the same class so that you dont need 4 seperate click events
+   //var score = yourScore + crystalValue;
+
+
+//start/reset game
+   //clear out variables (especially your score)
+   //set target score
+   //assign a value for each crystals
+   // .empty will be helpful
+   //If your score is grater than target score, then...
+   //If yourScore is less than, then... `
