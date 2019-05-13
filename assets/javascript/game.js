783d0e795de7386af4938711ff5c8eb7ac60a3f5
Mads <madisonmartinez@Alicias-MacBook-Pro.local>
2019-05-12 19:15:40 -0600

    cleaned up code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,9 +29,8 @@ function startGame() {
    targetScore = Math.floor((Math.random() * 120) + 19);
    $("#target-number").text(targetScore);
 
-   //assign values to each crystal 
+   //assign values to each crystal and make crystals
 
-   //display on the page target score and crystals
    for (var i = 0; i < crystal.length; i++) {
       var crystalValue = Math.floor((Math.random() * 12) + 1);
 
@@ -54,12 +53,13 @@ function reset() {
 
    $(".crystals").empty();
 
-   //make mew target number 
+   //make new target number 
 
    targetScore = Math.floor((Math.random() * 101) + 19);
    $("#target-number").text(targetScore);
 
    //display on the page target score and crystals
+
    for (var i = 0; i < crystal.length; i++) {
       var crystalValue = Math.floor((Math.random() * 12) + 1);
 
@@ -74,6 +74,8 @@ function reset() {
 
 
 //Let the clicking begin! 
+//Event delegation so things work the second time around
+
 
 $(document).on("click", ".crystal-image", function () {
 
@@ -94,57 +96,3 @@ $(document).on("click", ".crystal-image", function () {
       reset();
    }
 });
-
-
-
-
- // Next we create a for loop to create crystals for every numberOption.
-//  for (var i = 0; i < numberOptions.length; i++) {
-
-//    // For each iteration, we will create an imageCrystal
-//    var imageCrystal = $("<img>");
-
-//    // First each crystal will be given the class ".crystal-image".
-//    // This will allow the CSS to take effect.
-//    imageCrystal.addClass("crystal-image");
-
-//    // Each imageCrystal will be given a src link to the crystal image
-//    imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-
-//    // Each imageCrystal will be given a data attribute called data-crystalValue.
-//    // This data attribute will be set equal to the array value.
-//    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-//    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-//    $("#crystals").append(imageCrystal);
-//  }
-
-
-
-// //dynamically create crystals on the page (one object per crystal)
-//    //each crystal needs a score, an image, [aka a string holding the path to the image - which is saved in a folder in your project] an object with all of this info (aka collection of key value parts)
-
-// // Math.floor((Math.random() * 120) + 19); target number
-// // Math.floor((Math.random() * 12) + 1); crystal value
-// //crystalImage.attr("src", crystal.imageSrc);
-
-
-// // // Random number Function:
-// // function generateRandom()
-//    //something with math...
-//    //return a random number
-
-// //Click event function
-//    //each crystal should have the same class so that you dont need 4 seperate click events
-//    //var score = yourScore + crystalValue;
-
-
-// //start/reset game
-//    //clear out variables (especially your score)
-//    //set target score
-//    //assign a value for each crystals
-//    // .empty will be helpful
-//    //If your score is grater than target score, then...
-//    //If yourScore is less than, then... `
-
-//    //an array of crystals to generate maybe data-crystalValue
